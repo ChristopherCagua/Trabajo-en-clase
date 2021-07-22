b828533dc31f78b2681d81a881d5da3b0fcf3982
ChristopherCagua <caguachristopher@gmail.com>
2021-07-21 21:02:52 -0500

    ACTIVIDAD EN CLASE FUNCION RECURSIVA

---
+++ cmake-build-debug/CMakeFiles/Funcion_recursiva.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Funcion_recursiva.dir/main.cpp.obj"
+  "Funcion_recursiva.exe"
+  "Funcion_recursiva.exe.manifest"
+  "Funcion_recursiva.pdb"
+  "libFuncion_recursiva.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Funcion_recursiva.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Funcion_recursiva.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Funcion_recursiva.
+# This may be replaced when dependencies are built.
